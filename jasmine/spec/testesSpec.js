6f601687debec5f61adfd441d2295db4b33d20e3
danilo.romano <danilo.romano@azi.com.br>
2021-07-15 13:43:45 -0400

    demonstração dos Matchers.

---
+++ jasmine/spec/testesSpec.js
@@ -0,0 +1,12 @@
+describe('Testes (specs)',
+    function () {
+        it("Deve Garantir que 'true' é 'true'",
+            function () {
+
+            });
+
+        it("Deve garantir que 'true' não é 'false'",
+            function () {
+
+            });
+    })
---
+++ jasmine/spec/toBeFalsySpec.js
@@ -0,0 +1,12 @@
+describe("teste do toBeFalsy",function(){
+    it('deve demonstrar o uso do toBeFalsy',function(){
+        expect(null).toBeFalsy();
+        expect(undefined).toBeFalsy();
+        expect('').toBeFalsy();
+        expect(false).toBeFalsy();
+        expect(NaN).toBeFalsy();
+        expect(0).toBeFalsy();
+
+        expect(true).not.toBeFalsy();
+    })
+})
